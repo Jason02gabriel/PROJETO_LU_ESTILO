4a6d9bb0be074b1d8b6edd42c4f9a149243dd14a
Jason <j.gabrielaraujodasilva@gmail.com>
2022-09-29 21:38:17 -0300

    finalizado a pagina de detalhe dos produtos

--- js/teste.js
+++ js/teste.js
@@ -13,3 +13,26 @@ if (close) {
         nav.classList.remove('active');
     });
 }
+
+ function product1() {
+         window.location.href = "produto.html";
+        }
+ function product2() {
+         window.location.href = "produto2.html";
+        }
+ function product3() {
+         window.location.href = "produto.html";
+        }
+ function product4() {
+         window.location.href = "produto.html";
+        }
+ function product5() {
+         window.location.href = "produto.html";
+        }
+ function product6() {
+         window.location.href = "produto.html";
+        }
+ function product7() {
+         window.location.href = "produto7.html";
+        }
+
